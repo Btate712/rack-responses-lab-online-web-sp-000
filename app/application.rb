c5966ee14be299627763e419463d8fe2de0e74a4
Robert Tate <btate712@gmail.com>
2019-04-15 15:22:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,9 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    current_time = Time.now
-    current_hour = current_time.hour
-    resp.write current_hour > 11? "Good Morning!" : "Good Afternoon!"
+    resp.write Time.now.hour > 11? "Good Morning!" : "Good Afternoon!"
     resp.finish
   end
 end
