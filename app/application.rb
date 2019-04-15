8e697cd2ff502e39f5a6ac0d9dfe73d956e7859c
Robert Tate <btate712@gmail.com>
2019-04-15 15:26:33 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    current_time = Time.now
+    current_time = Time.now.localtime
     resp.write "Current time is #{current_time.hour}:#{current_time.min}\n"
     resp.write current_time.hour > 11? "Good Morning!" : "Good Afternoon!"
     resp.finish
